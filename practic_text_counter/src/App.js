3ffc809aa6833549925d2a44b98f94c334b1a6a8
Vishaltandale987 <tandalevishal987@gmail.com>
2022-10-06 08:22:47 +0530

    aa

---
+++ practic_text_counter/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './conponts/Navbar.js';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      
+    </div>
+    <Navbar/>
+    </>
+  );
+}
+
+export default App;
